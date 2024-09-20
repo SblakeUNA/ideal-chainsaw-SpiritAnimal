08fc198f83ec59cd1f1218c4aed4222a8afdda85
SblakeUNA <163796752+SblakeUNA@users.noreply.github.com>
2024-09-20 16:43:29 -0500

    Add files via upload

---
+++ Scripts/script.js
@@ -0,0 +1,14 @@
+//$('body').css("background-color", "yellow");
+// when button is clicked
+// IIFEW = immediatley invoked f/n expression
+$("#btnGetAnimal").on('click', function(){
+// get the user name
+  let bakername = $('#bakername').val();
+//   var variablename = ... let(const)
+   $("#greetingArea").text("Hi there, " + bakername + "!");
+// give them greeting
+// next. also tell the spirit animal
+   let randomanimal = spiritanimals[(Math.floor(Math.random() * spiritanimals.length))];
+  $("#animalarea").text("Your spirit animal is: " + randomanimal)
+})
+ let spiritanimals = ["🫎","🫏","🦈","🐟","🪱","🐔","🦆"]
